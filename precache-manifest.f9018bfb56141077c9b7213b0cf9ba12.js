fc5fc192676a71501e5a634a76eecf6b7b976611
pa-volpin <paulo.volpin@gmail.com>
2021-01-11 17:41:21 -0300

    Updates

---
+++ precache-manifest.f9018bfb56141077c9b7213b0cf9ba12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79839b54d152dde09d3a1738a0f96ef",
+    "url": "/app-pokedex/index.html"
+  },
+  {
+    "revision": "93ebd44c5865e8b1504d",
+    "url": "/app-pokedex/static/css/main.0ee91d88.chunk.css"
+  },
+  {
+    "revision": "6992fc859e24464dfc41",
+    "url": "/app-pokedex/static/js/2.2cf2e2cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-pokedex/static/js/2.2cf2e2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ebd44c5865e8b1504d",
+    "url": "/app-pokedex/static/js/main.28ffa79d.chunk.js"
+  },
+  {
+    "revision": "2cc25761102584cc339b",
+    "url": "/app-pokedex/static/js/runtime-main.470738a9.js"
+  },
+  {
+    "revision": "e39b31eeddaf1884b8ce09c49aefb7ed",
+    "url": "/app-pokedex/static/media/pokeball.e39b31ee.png"
+  }
+]);
